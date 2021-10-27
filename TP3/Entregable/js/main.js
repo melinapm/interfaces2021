ae8b63ea74984105427708738f98b5e308896400
Melina Perez Mounet <meli.pm@gmail.com>
2021-10-27 13:39:47 -0300

    Se agrega sprite de salto, muerte. Manejo de vida, obstaculos. Ganador y perdedor

--- TP3/Entregable/js/main.js
+++ TP3/Entregable/js/main.js
@@ -1,61 +1,125 @@
-// Variables globales
-const canvas = document.getElementById('canvas1');
-const ctx = canvas.getContext('2d');
-const CANVAS_WIDTH = canvas.width = 1000;
-const CANVAS_HEIGHT = canvas.height = 700;
-let gameSpeed = 0;
-
-// Fondos
-const backgroundLayer1 = new Image();
-backgroundLayer1.src = 'images/layer-1.png';
-const backgroundLayer2= new Image();
-backgroundLayer2.src = 'images/layer-2.png';
-const backgroundLayer3 = new Image();
-backgroundLayer3.src = 'images/layer-3.png';
-
-// Trooper
-let playerImage = new Image();
-playerImage.src = 'images/walk.png';
-
-
-
 $('#modalPrincipal').modal({backdrop: 'static', keyboard: false})
 $("#modalPrincipal").modal('show');
 
-// Cuando tengo todo cargado
-window.addEventListener('load', function() {
+// Arrancamos a jugar!
+let cantidadVidas;
+let cantidadPiedras;
+let trooperYInit = document.getElementById('trooper').getBoundingClientRect().y;
+let validatorGame;
+let botonesVolverAJugar = document.querySelectorAll("#volverAJugar");
+let divCantObstaculos = document.getElementById('cantObstaculos');
 
-    // Arrancamos a jugar!
-    document.addEventListener('keydown', (event) => {
-        $("#modalPrincipal").modal('hide');
-        var name = event.key;
-        if (name == 'ArrowRight') {
-            staggerFrames = 5;
-            gameSpeed = 10;
-        }
-    }, false);
-
-    const layer1 = new Layer(backgroundLayer1, 0.3);
-    const layer2 = new Layer(backgroundLayer2, 0.5);
-    const layer3 = new Layer(backgroundLayer3, 0.8);
-    
-    const gameObject = [layer1, layer2, layer3];
-    
-    const tropper = new Tropper(playerImage);
-
-    // Funcion para mover el fondo
-    function animateBackground() {
-        ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT); // Reseteo el context asi no arrastra la imagen
-        gameObject.forEach(object => {
-            object.update();
-            object.draw();
-        });
-        requestAnimationFrame(animateBackground);
-    };
-
-    animateBackground();
-    tropper.animate();
+botonesVolverAJugar.forEach(function(boton) {
+    boton.addEventListener("click", modalsGame);
 });
 
+function modalsGame(){
+    $("#modalPerdiste").modal('hide');
+    $("#modalGanaste").modal('hide');
+    startAnimations();
+    startGame();
+};
+  
+document.addEventListener('keydown', (event) => {
+    
+    var name = event.key;
+    switch (name) {
+        case 'ArrowRight':
+            $("#modalPrincipal").modal('hide');
+            startAnimations();
+            startGame();
+            break;
+        case 'ArrowUp':
+            document.getElementById('trooper').classList.remove('caminar');
+            document.getElementById('trooper').classList.add('saltar');
+            setTimeout(function() {
+                document.getElementById('trooper').classList.remove('saltar');
+                document.getElementById('trooper').classList.add('caminar');
+            }, 100);
+            break;
+    }
+}, false);
+
+function startGame(){
+    validatorGame = setInterval(function() {
+    
+        var piedraX = document.getElementById('obstaculo').getBoundingClientRect().x; // disminuye hasta el trooper
+
+        var trooperX = document.getElementById('trooper').getBoundingClientRect().x; // Siempre igual
+        var trooperY = document.getElementById('trooper').getBoundingClientRect().y; // Siempre igual menos cuando salta que resta
+        
+        if (((trooperX+35) >= piedraX) && (trooperY ==  trooperYInit)) {
+            cantidadVidas--;
+            changeLifes();
+            document.getElementById('trooper').classList.remove('caminar');
+            document.getElementById('trooper').classList.add('morir');
+            setTimeout(function() {
+                document.getElementById('trooper').classList.remove('morir');
+                document.getElementById('trooper').classList.add('caminar');
+            }, 1000);} 
+        else if (((trooperX+35) >= piedraX) && (trooperY <  trooperYInit)) {
+            cantidadPiedras --;
+            divCantObstaculos.innerHTML = cantidadPiedras;
+        }
+        
+
+        if (cantidadVidas == 0) {
+            endGame();
+        }
+        if (cantidadPiedras == 0) {
+            winGame();
+        }
+
+    }, 100);
+}
+
+function endGame(){
+    $('#modalPerdiste').modal({backdrop: 'static', keyboard: false})
+    $("#modalPerdiste").modal('show');
+    clearInterval(validatorGame);
+    stopAnimations();
+}
+
+function changeLifes(){
+    let vidas = document.getElementById('vidas');
+    switch (cantidadVidas) {
+        case 3:
+            vidas.style.background = 'url(images/lifes.png) 0px 30px';
+            break;
+        case 2:
+            vidas.style.background = 'url(images/lifes.png) 0px 70px';
+            break;
+        case 1:
+            vidas.style.background = 'url(images/lifes.png) 0px 110px';
+            break;
+        case 0:
+            vidas.style.background = 'url(images/lifes.png) 0px 150px';
+            break;
+    }
+}
+
+function stopAnimations(){
+    var animations = document.getElementsByClassName("layer");
+    for (var i = 0; i < animations.length; i++) {
+        animations.item(i).classList.add('remove-animation');;
+    }
+}
+
+function startAnimations(){
+    cantidadVidas = 3;
+    cantidadPiedras = 10;
+    divCantObstaculos.innerHTML = cantidadPiedras;
+    changeLifes();
+    var animations = document.getElementsByClassName("layer");
+    for (var i = 0; i < animations.length; i++) {
+        animations.item(i).classList.remove('remove-animation');;
+    }
+}
 
 
+function winGame(){
+    $('#modalGanaste').modal({backdrop: 'static', keyboard: false})
+    $("#modalGanaste").modal('show');
+    clearInterval(validatorGame);
+    stopAnimations();
+}
