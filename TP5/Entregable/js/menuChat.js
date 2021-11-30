4d080f547504350f1985cf5749003a71b7575e54
Melina Perez Mounet <meli.pm@gmail.com>
2021-11-30 20:22:35 -0300

    Cambios en el chat

--- TP5/Entregable/js/menuChat.js
+++ TP5/Entregable/js/menuChat.js
@@ -3,15 +3,15 @@
 /// Menu con JQuery
 $(document).ready(function(){
      $("#chat").click(function(e){
-      e.preventDefault();
-      $('#div_principal').load('chat.html',function() {
-          $("#div_principal").fadeIn(800);
-        });
+          e.preventDefault();
+          $('#div_principal').load('chat.html',function() {
+               $("#div_principal").fadeIn(800);
+          });
      })
      $("#chatTodos").click(function(e){
           e.preventDefault();
           $('#div_principal').load('chat.html',function() {
-              $("#div_principal").fadeIn(800);
-            });
-         })
+               $("#div_principal").fadeIn(800);
+               });
+          })
 });
