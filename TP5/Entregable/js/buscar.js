c7e5cdb0522b4ba8043a70770b299b753a8dbd60
Melina Perez Mounet <meli.pm@gmail.com>
2021-12-02 17:49:08 -0300

    Resultados en el buscar

---
+++ TP5/Entregable/js/buscar.js
@@ -0,0 +1,11 @@
+"use strict";
+
+document.addEventListener('keydown', (event) => {
+    
+     var name = event.key;
+     if (name == "Enter") {
+          document.getElementById("resultadosBusquedaJose").style.display = "block"; 
+     }
+
+     }
+);
