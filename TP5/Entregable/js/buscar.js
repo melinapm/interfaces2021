ee8999fa2026415a3b3e518087b0d402c2aacf0e
Melina Perez Mounet <meli.pm@gmail.com>
2021-12-03 10:54:38 -0300

    Carga en los resultados de busqueda

--- TP5/Entregable/js/buscar.js
+++ TP5/Entregable/js/buscar.js
@@ -12,8 +12,20 @@ document.addEventListener('keydown', (event) => {
     
      var name = event.key;
      if (name == "Enter") {
-          document.getElementById("resultadosBusquedaJose").style.display = "block"; 
+          cargaSpinner();
+          setTimeout(muestraResultados, 2000); 
      }
 
-     }
-);
+});
+
+
+function cargaSpinner() {
+     document.getElementById("spinnerResultado").innerHTML = "<br><br><br> <div class=\"d-flex justify-content-center\"><div class=\"spinner-border text-info\" role=\"status\"><span class=\"sr-only\">Cargando...</span></div></div>";
+};
+
+function muestraResultados() {
+     document.getElementById("spinnerResultado").innerHTML = "";
+     document.getElementById("resultadosBusquedaJose").style.display = "block"; 
+};
+
+
